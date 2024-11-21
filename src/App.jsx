3a8fa03dfe09248081626202b5a4f148a8ac8a53
frontendoniyorbek <tursunovdoniyorbek077@gmail.com>
2024-11-21 10:48:59 +0500

    site Be Part of the Future Tech Revolution page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import FutureTech from './components/ FutureTech';
+// import ResponsiveTailwid from './components/ResponsiveTailwid';
+
+function App() {
+	return (
+		<>
+			<FutureTech />
+			{/* <ResponsiveTailwid /> */}
+		</>
+	);
+}
+
+export default App;
